84cb37ec02907b8e6c861f5d976f3772f17b33d9
vedantswain <vedant12118@iiitd.ac.in>
2015-10-05 11:09:50 +0530

    integrated static html and added templates

--- police_dashboard/tool/views.py
+++ police_dashboard/tool/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tool index.")
+
+def dashboard(request,handle):
+	template = loader.get_template('tool/basic.html')
+	context = RequestContext(request, {
+	    'dashboard_name': handle+" dashboard",
+	})
+	return HttpResponse(template.render(context))
